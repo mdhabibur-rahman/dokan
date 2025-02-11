8028fc2b6e6c5ff640b15d1226fe15ab6705986f
mdhabibur-rahman <145204698+mdhabibur-rahman@users.noreply.github.com>
2025-02-11 11:23:20 +0600

    add

--- src/Pages/Component/More/More.jsx
+++ src/Pages/Component/More/More.jsx
@@ -21,19 +21,22 @@ const More = ({ toggleModal }) => {
             </button>
 
             {/* Dropdown Menu */}
-            {isDropdownOpen && (
-                <div className="absolute right-[-8px] bg-white shadow-lg rounded-b-md mt-60 w-48 p-2 flex flex-col">
-                    <a href="/" className="py-2 px-4 hover:bg-gray-200 rounded-md">Home</a>
-                    <a href="/about" className="py-2 px-4 hover:bg-gray-200 rounded-md">About Us</a>
-                    <a href="/login" className="py-2 px-4 hover:bg-gray-200 rounded-md">Login</a>
-                    <a
-                        className="py-2 px-4 hover:bg-gray-200 rounded-md"
-                        onClick={toggleModal}
-                    >
-                        Sign Up
-                    </a>
-                </div>
-            )}
+            <div
+                className={`absolute right-[-8px] bg-white shadow-lg rounded-b-md mt-60 w-48 p-2 flex flex-col transition-all duration-700 ease-in-out ${isDropdownOpen
+                    ? 'opacity-100 transform translate-y-0 scale-100 blur-none'
+                    : 'opacity-0 transform translate-y-4 scale-95 blur-sm pointer-events-none'
+                    }`}
+            >
+                <a href="/" className="py-2 px-4 hover:bg-gray-200 rounded-md">Home</a>
+                <a href="/about" className="py-2 px-4 hover:bg-gray-200 rounded-md">About Us</a>
+                <a href="/login" className="py-2 px-4 hover:bg-gray-200 rounded-md">Login</a>
+                <a
+                    className="py-2 px-4 hover:bg-gray-200 rounded-md"
+                    onClick={toggleModal}
+                >
+                    Sign Up
+                </a>
+            </div>
         </div>
     );
 };
